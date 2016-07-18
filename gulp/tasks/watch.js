094cc01d7fa54f919fad470c2d9b606fbc4e31ec
Kristy Nolan <kristy.walters@connect-group.com>
2016-07-18 10:09:13 +0100

    fixed wrong task name in watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,7 +8,7 @@ var config = require('../config');
 
 gulp.task('watch', ['build', 'watchify', 'serve'], function() {
     // .scss
-    gulp.watch(config.css.watchSrc, ['sass']);
+    gulp.watch(config.css.watchSrc, ['css']);
 
     // .html
     gulp.watch(config.html.path + '/**/*.{html,hbs}', ['html']);
